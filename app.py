c610276dace71ff28b80cda4ee286d030a77ad5e
AngKuKuehs <bryananghe.dev@gmail.com>
2024-02-18 22:22:55 -0500

    Added route /delete-review

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ load_dotenv()
 MONGODB_URI = os.environ["MONGODB_URI"]
 client = MongoClient(MONGODB_URI)
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def default_route():
     """
     Returns a string
     """
-    return 'Landing page for to-do-list project'
+    return "Landing page for to-do-list project"
 
-@app.route('/get-all', methods=['GET'])
+@app.route("/get-all", methods=["GET"])
 def get_all_dates():
     """
     Gets all date entries.
@@ -44,7 +44,7 @@ def get_all_dates():
         result.append(document_cleaned)
     return result
 
-@app.route('/get-one', methods=['GET'])
+@app.route("/get-one", methods=["GET"])
 def get_one_reminder():
     """
     Gets a date entry.
@@ -59,14 +59,14 @@ def get_one_reminder():
     dict
         Details of date entry.
     """
-    oid = request.args.get('oid')
+    oid = request.args.get("oid")
     db = client["dates-in-sg"]
     dates_collection = db.dates
     document_to_find = {"_id": ObjectId(oid)}
     result = dates_collection.find_one(document_to_find)
     return json.loads(json_util.dumps(result))
 
-@app.route('/add-date', methods=['POST'])
+@app.route("/add-date", methods=["POST"])
 def add_date_entry():
     """
     Adds a date entry.
@@ -93,7 +93,7 @@ def add_date_entry():
         return f"_id of inserted document: {result.inserted_id}"
     return "review does not conform to schema"
 
-@app.route('/update-date', methods=['PUT'])
+@app.route("/update-date", methods=["PUT"])
 def edit_date_entry():
     """
     Updates a date entry.
@@ -115,7 +115,7 @@ def edit_date_entry():
     """
     db = client["dates-in-sg"]
     dates_collection = db.dates
-    oid = request.args.get('oid')
+    oid = request.args.get("oid")
 
     reminder_to_update = {"_id": ObjectId(oid)}
     updates = request.get_json()
@@ -124,7 +124,7 @@ def edit_date_entry():
 
     return f"Documents updated: {str(result.modified_count)}"
 
-@app.route('/remove-date', methods=['DELETE'])
+@app.route("/remove-date", methods=["DELETE"])
 def remove_date_entry():
     """
     Deletes a date entry.
@@ -141,13 +141,13 @@ def remove_date_entry():
     """
     db = client["dates-in-sg"]
     dates_collection = db.dates
-    oid = request.args.get('oid')
+    oid = request.args.get("oid")
     document_to_delete = {"_id": ObjectId(oid)}
 
     result = dates_collection.delete_one(document_to_delete)
     return f"Documents deleted: {str(result.deleted_count)}"
 
-@app.route('/add-review', methods=['PUT'])
+@app.route("/add-review", methods=["PUT"])
 def add_date_review():
     """
     Adds a review to a specified date entry.
@@ -170,18 +170,18 @@ def add_date_review():
     db = client["dates-in-sg"]
     dates_collection = db.dates
 
-    oid = request.args.get('oid')
+    oid = request.args.get("oid")
     date_review = request.get_json()
     date_review = dict(date_review)
     date_review["date_added"] = datetime.datetime.now()
     date_to_review = {"_id": ObjectId(oid),}
     result = dates_collection.update_one(date_to_review,
-                                         {'$push': {'reviews': date_review}})
+                                         {"$push": {"reviews": date_review}})
     return f"Reviews added: {str(result.modified_count)}"
 
 
-@app.route('/update-review', methods=['PUT'])
-def update_date_review():
+@app.route("/update-review", methods=["PUT"])
+def update_review():
     """
     Adds a review to a specified date entry.
 
@@ -210,9 +210,34 @@ def update_date_review():
     date_review["date_added"] = datetime.datetime.now()
     date_to_review = {"_id": ObjectId(oid), "reviews.user_id": user_id}
     result = dates_collection.update_one(date_to_review,
-                                         {'$set': {'reviews.$': date_review}})
+                                         {"$set": {"reviews.$": date_review}})
     return f"Reviews modified: {str(result.modified_count)}"
 
+@app.route("/delete-review", methods=["DELETE"])
+def delete_review():
+    """
+    Deletes a review to a specified date entry.
+
+    Query Parameters
+    ----------
+    oid: str
+        id_ of date entry to update.
+    uid: str
+        id_ of the user whose review is to be deleted.
+
+    Returns
+    -------
+    str
+        Number of documents updated.
+    """
+    db = client["dates-in-sg"]
+    dates_collection = db.dates
+    oid = request.args.get("oid")
+    user_id = request.args.get("uid")
+    review_to_delete = {"$pull": {"reviews": {"user_id": user_id}}}
+
+    result = dates_collection.update_one({"_id": ObjectId(oid)}, review_to_delete)
+    return f"Reviews deleted: {str(result.modified_count)}"
 
 if __name__ == "__main__":
     app.run()
