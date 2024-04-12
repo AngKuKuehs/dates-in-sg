eaa546590ab6255ac273a5a48420965acb285eb2
AngKuKuehs <bryananghe.dev@gmail.com>
2024-04-12 16:18:43 -0400

    Added try-except

--- server/src/db.py
+++ server/src/db.py
@@ -4,11 +4,15 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 import mongomock
 
-load_dotenv()
-MONGODB_URI = os.environ["MONGODB_URI"]
-client = MongoClient(MONGODB_URI)
-db = client["dates-in-sg"]
-dates_collection = db.dates
+try:
+    load_dotenv()
+    MONGODB_URI = os.environ["MONGODB_URI"]
+    client = MongoClient(MONGODB_URI)
+    db = client["dates-in-sg"]
+    dates_collection = db.dates
+except:
+    print("invalid/missing MONGODB_URI env var")
+    dates_collection = None
 
 test_collection = mongomock.MongoClient().db.collection
 
